70071738fd5d02a3c7ffcba7690e768ecf329e90
Andrew McDonough <andrew@andrewmcdonough.co.uk>
2011-02-14 11:53:06 +0000

    Date calculations should be correct

--- lighthouse.rb
+++ lighthouse.rb
@@ -1,5 +1,24 @@
 require 'sinatra'
 
 get '/' do
-  "hi"
+  @who = who_is_on_lighthouse(Time.now)
+  erb :index
 end
+
+def who_is_on_lighthouse(date)
+  who_is_on_lighthouse_on_day(days_since_start(date))
+end
+
+def who_is_on_lighthouse_on_day(day)
+  developers = %w(Andrew Simon Joe)
+  (1..day).each do |i|
+    next if [1,2].include? i % 7
+    developers.rotate!
+  end
+  return developers[0]
+end
+
+def days_since_start(date)
+  ((date - Time.mktime(2011,02,04)) / (60*60*24)).floor
+end
+
